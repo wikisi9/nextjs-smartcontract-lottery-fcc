aa844bdb3882048a9a95cabbd85df47b8b1f2384
DefiJack <wikisi9@gmail.com>
2023-03-13 14:05:39 -0500

    changes

--- components/ManualHeader.js
+++ components/ManualHeader.js
@@ -1,6 +1,53 @@
 import { useMoralis } from "react-moralis"
+import { useEffect } from "react"
 
 export default function ManualHeader() {
-    const { enableWeb3 } = useMoralis()
-    return <div>Hi from Header!</div>
+    const { enableWeb3, account, isWeb3Enabled, Moralis, deactivateWeb3, isWeb3EnableLoading } =
+        useMoralis()
+
+    useEffect(() => {
+        if (isWeb3Enabled) return
+        if (typeof window !== "undefined") {
+            if (window.localStorage.getItem("connected")) {
+                enableWeb3()
+            }
+        }
+    }, [isWeb3Enabled])
+
+    // If array above is empty, useEffect will run function once whenever something changes in the project.
+    // if array contains variable, useEffect will run function once whenever something in the array changes.
+    // if we send no array at all, function will run anytime something re-renders
+
+    useEffect(() => {
+        Moralis.onAccountChanged((account) => {
+            console.log(`account changed to ${account}`)
+            if (account == null) {
+                window.localStorage.removeItem("connected")
+                deactivateWeb3()
+                console.log("null account found!")
+            }
+        })
+    }, [])
+
+    return (
+        <div>
+            {account ? (
+                <div>
+                    Connected to {account.slice(0, 6)}...{account.slice(account.length - 4)}
+                </div>
+            ) : (
+                <button
+                    onClick={async () => {
+                        await enableWeb3()
+                        if (typeof window !== "undefined") {
+                            window.localStorage.setItem("connected", "injected")
+                        }
+                    }}
+                    disabled={isWeb3EnableLoading}
+                >
+                    Connect
+                </button>
+            )}
+        </div>
+    )
 }
